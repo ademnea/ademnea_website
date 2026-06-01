2b8b302ef63db6daf0d55953250e5d74cbd5d89f
OkemaPaulMark <paulokema342@gmail.com>
2026-06-01 12:46:51 +0300

    added

--- resources/views/vendor/laravelusers/usersmanagement/show-users.blade.php
+++ resources/views/vendor/laravelusers/usersmanagement/show-users.blade.php
@@ -26,6 +26,7 @@
     <table id="myTable" class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
+                <th scope="col" class="px-6 py-3">#</th>
                 <th scope="col" class="px-6 py-3">Name</th>
                 <th scope="col" class="px-6 py-3">Email</th>
                 <th scope="col" class="px-6 py-3">Created</th>
@@ -36,6 +37,7 @@
         <tbody>
             @foreach($users as $user)
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">{{ $loop->iteration }}</td>
                 <td class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
                     <div class="w-10 h-10 rounded-full bg-green-700 flex items-center justify-center text-white font-bold text-sm">
                         {{ strtoupper(substr($user->name, 0, 1)) }}
@@ -140,7 +142,7 @@ $(document).ready(function() {
         paging: true,
         pageLength: 10,
         lengthMenu: [5, 10, 25, 50],
-        dom: 'lrtip',  // hide default search box, we use our own
+        dom: 'rtip',
         language: {
             paginate: {
                 previous: 'Prev',
